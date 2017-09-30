e7efe7afb1a0469d2482cc5a8f75d226e2ee62b6
Yuma Okabe <rozabmmsr@gmail.com>
2017-09-30 12:55:01 +0000

    change layouts basic pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def fill_title(page_title)
+        base_title =  "Movie SNS"
+        if page_title.empty?
+            base_title
+        else
+            "#{page_title} | #{base_title} "
+        end
+    end
 end
