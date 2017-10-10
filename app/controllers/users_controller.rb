b1f3d37213d33a93a562ab2af7dc2e63f11d519e
Yuma Okabe <rozabmmsr@gmail.com>
2017-10-10 15:36:37 +0000

    Finish user edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     # @user = User.find(1)
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Movie SNS!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:use).permit(:name, :email, :password, :password_confirmation)
+  end
+  
 end
